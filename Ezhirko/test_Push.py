21aeb8546227be357a9f47e911a79e5c595e256d
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2021-09-24 01:21:50 +0530

    Update test_Push.py
    Corrected the path

--- Ezhirko/test_Push.py
+++ Ezhirko/test_Push.py
@@ -13,18 +13,18 @@ def test_model_file_not_present():
 
 # your accuracy of the model is more than 70%
 def test_model_accuracy_above_seventy():
-    metrix_df = pd.read_csv('Metrics.csv')
+    metrix_df = pd.read_csv('Ezhirko/Metrics.csv')
     acc = max(metrix_df['TestAccuracy'])
     assert acc > 70.0
 
 # your accuracy of the cat class is more than 70%
 def test_cat_class_accuracy_above_seventy():
-    metrix_df = pd.read_csv('Metrics.csv')
+    metrix_df = pd.read_csv('Ezhirko/Metrics.csv')
     acc = max(metrix_df['CatAccuracy'])
     assert acc > 70.0
     
 # your accuracy of the dog class is more than 70%
 def test_dog_class_accuracy_above_seventy():
-    metrix_df = pd.read_csv('Metrics.csv')
+    metrix_df = pd.read_csv('Ezhirko/Metrics.csv')
     acc = max(metrix_df['DogAccuracy'])
     assert acc > 70.0
