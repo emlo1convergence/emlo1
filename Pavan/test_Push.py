d28d9f35c0a3b9592520954c178ed5667ca753a0
Naga Pavan Kumar <nagapavan.kalepu@gmail.com>
2021-09-24 20:31:04 +0530

    adding unit tests

---
+++ Pavan/test_Push.py
@@ -0,0 +1,31 @@
+
+import pytest
+import pandas as pd
+from gitignore_parser import parse_gitignore
+
+matches = parse_gitignore('Pavan/.gitignore')
+# you have NOT uploaded data
+def test_data_folder_not_present():
+    assert matches('Pavan/data')
+
+# you have NOT uploaded best-model-parameters.pt
+def test_model_file_not_present():
+    assert matches('Pavan/best-model-parameters.pt')
+
+# your accuracy of the model is more than 70%
+def test_model_accuracy_above_seventy():
+    metrix_df = pd.read_csv('Pavan/Metrics.csv')
+    acc = max(metrix_df['TestAccuracy'])
+    assert acc > 70.0
+
+# your accuracy of the cat class is more than 70%
+def test_cat_class_accuracy_above_seventy():
+    metrix_df = pd.read_csv('Pavan/Metrics.csv')
+    acc = max(metrix_df['CatAccuracy'])
+    assert acc > 70.0
+    
+# your accuracy of the dog class is more than 70%
+def test_dog_class_accuracy_above_seventy():
+    metrix_df = pd.read_csv('Pavan/Metrics.csv')
+    acc = max(metrix_df['DogAccuracy'])
+    assert acc > 70.0
